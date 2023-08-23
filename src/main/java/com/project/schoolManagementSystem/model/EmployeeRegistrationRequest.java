41b1f2bed49d4b6e382e433af5e8a2a63c198a10
user <toropov.nikita2004@gmail.com>
2023-08-23 10:11:28 +0200

    feat: add a toString method and setters

--- src/main/java/com/project/schoolManagementSystem/model/EmployeeRegistrationRequest.java
+++ src/main/java/com/project/schoolManagementSystem/model/EmployeeRegistrationRequest.java
@@ -2,10 +2,14 @@ package com.project.schoolManagementSystem.model;
 
 
 import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDate;
 
+@ToString(callSuper = true)
 @Getter
+@Setter
 public class EmployeeRegistrationRequest extends RegistrationRequest {
     private int salary;
     private LocalDate hireDate;
